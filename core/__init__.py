88cfde44ddb25ad7a2c01146c0a74cfc9c11a3e4
azbutton <262894401+test-reports@users.noreply.github.com>
2026-03-13 12:57:02 -0700

    Removed extra files

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Core utilities for CLI test automation."""
-
-from core.command_runner import run_cli
-
-__all__ = ["run_cli"]
--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility modules for CLI test automation."""
-
-from utils.logger import get_logger
-
-__all__ = ["get_logger"]
